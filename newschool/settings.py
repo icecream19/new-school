91839c0781e4dcc1fb6b16e6998fd5805a44b8d1
Rafsun <rafsunofficial@gmail.com>
2020-04-27 13:52:42 +0600

    production init and requrements.txt

--- newschool/settings.py
+++ newschool/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ljdb)n9b-(x%*toflm21gye2)30r52hx3&g(+eb5@4ns$9g&55'
+# SECRET_KEY = 'ljdb)n9b-(x%*toflm21gye2)30r52hx3&g(+eb5@4ns$9g&55'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'ljdb)n9b-(x%*toflm21gye2)30r52hx3&g(+eb5@4ns$9g&55')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
